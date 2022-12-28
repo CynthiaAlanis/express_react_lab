346148189b9620f0daebc8051fbf5388e4f12f61
cynthia <santoro.cynthia@yahoo.com>
2022-12-27 21:12:38 -0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer(props) {
+  return <h5>December 20, 2022 | Cynthia Alanis</h5>;
+}
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+function Home(props) {
+  return(
+    
+  <img src="https://media4.giphy.com/media/9JrkkDoJuU0FbdbUZU/giphy.gif?cid=ecf05e476ks9v1usb1o05muay26iniidrw1k6gkeyieimjs9&rid=giphy.gif&ct=g" alt="welcome" />)
+;
+}
+
+export default Home;
